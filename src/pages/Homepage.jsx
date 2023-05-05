c77c93c7a3f88ac1fd91a2c1787abc14e87b6ecd
kadiryaren <uraniumstar20@gmail.com>
2023-05-06 01:24:05 +0300

    Project architecture created

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from '../styles/Main.module.css';
+
+export default function Homepage() {
+  return <div className={`${style.container} + text-5xl`}>Homepage</div>;
+}
